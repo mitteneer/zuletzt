51df37aa21a1e63f9c3892918d961eb85490ebb3
mitteneer <swebbguy@gmail.com>
2024-11-21 18:45:13 -0500

    Create migrations

---
+++ src/app/database/migrations/2024-11-21_21-52-55_create_table.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const jetquery = @import("jetquery");
+const t = jetquery.schema.table;
+
+pub fn up(repo: anytype) !void {
+    try repo.createTable(
+        "concerts",
+        &.{
+            t.primaryKey("id", .{}),
+            t.column("date", .datetime, .{}),
+            t.column("location", .string, .{}),
+            t.timestamps(.{}),
+        },
+        .{},
+    );
+}
+
+pub fn down(repo: anytype) !void {
+    try repo.dropTable("concerts", .{});
+}
